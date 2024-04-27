b77c1517c4e82882bbe9bbadc011852eeed5e1d1
Sofia <166915743+Sofu17@users.noreply.github.com>
2024-04-27 10:39:25 -0300

    Sofia Exercicio5.java

--- src/main/java/competec/aula1_ramificacoes/Exercicio5.java
+++ src/main/java/competec/aula1_ramificacoes/Exercicio5.java
@@ -5,7 +5,13 @@ package competec.aula1_ramificacoes;
 public class Exercicio5 {
     public static void main(String[] args) {
         // Escreva seu código aqui ⬇️
-
+Scanner scanner = new Scanner(System.in);
+        int Numero = scanner.nextInt(10);
+        if (numero % 5 == 0) || (numero % 3 == 0);
+        System.out.printIn("verdadeiro"); 
+        else{
+        System.out.printIn("falso");
+            
 
     }
 }
